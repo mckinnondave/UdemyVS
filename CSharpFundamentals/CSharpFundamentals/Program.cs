01a4678f58ec68083ae46e3f68105a3d3fe683d8
dave mckinnon <dave.w.mckinnon@gmail.com>
2022-08-24 18:34:30 -0700

    add program for classes with multiple created files

---
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,20 @@
+﻿using CSharpFundamentals.Math;
+using System;
+
+namespace CSharpFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
