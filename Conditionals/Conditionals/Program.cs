b555e016410d753c092660a33adbdd48f53a5c68
dave mckinnon <dave.w.mckinnon@gmail.com>
2022-08-27 12:06:56 -0700

    add program for conditionals

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Conditionals
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                case Season.Summer:
+                    System.Console.WriteLine("We've got a promotion!");
+                    break ;
+
+                default:
+                    System.Console.WriteLine("I don't understand this season");
+                    break;
+            }
+        }
+    }
+}
