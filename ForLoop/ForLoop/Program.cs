465ab52f1c593a750205d9ed2115d869f48ebbb3
dave mckinnon <dave.w.mckinnon@gmail.com>
2022-09-01 10:44:27 -0700

    add program showing various loop methods

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Program
+    {
+        // FOR LOOP
+        /*static void Main(string[] args)
+        {
+            for (var i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (int i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }*/
+
+        // FOR LOOP AND FOREACH
+        /*static void Main(string[] args)
+        {
+            var name = "John Smith";
+
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+        }*/
+        
+        // FOREACH
+       /* static void Main(string[] args)
+        {
+            var numbers = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }*/
+
+        // WHILE
+        /*static void Main(string[] args)
+        {
+            var i = 0;
+            while (i <= 10)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+                    i++;
+            }
+        }*/
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+
+                Console.WriteLine("@Echo: " + input);
+            }
+        }
+    }
+}
